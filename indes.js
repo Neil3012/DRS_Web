6ebd2e7487f315f2aabe7fed415a59f74579ee16
Neil3012 <63674799+Neil3012@users.noreply.github.com>
2023-10-03 16:14:25 +0530

    af

--- indes.js
+++ indes.js
@@ -1,9 +1,9 @@
-// Get a reference to the AR object and UI elements
-const arObject = document.querySelector('#boxID');
-const uiElement = document.querySelector('#ar-ui');
+// Get a reference to the AR object and tag element
+const arObject = document.querySelector('#boxId');
+const arTag = document.querySelector('#ar-tag');
 
-// Function to update the UI position
-function updateUIPosition() {
+// Function to update the tag position
+function updateTagPosition() {
     const arObjectPosition = arObject.object3D.position;
     const arObjectWorldPos = new THREE.Vector3();
     arObject.object3D.getWorldPosition(arObjectWorldPos);
@@ -11,14 +11,14 @@ function updateUIPosition() {
     // Convert world coordinates to screen coordinates
     const screenPosition = arjs.camera.project(arObjectWorldPos);
 
-    // Set the position of the UI element
-    uiElement.style.top = `${screenPosition.y}px`;
-    uiElement.style.left = `${screenPosition.x}px`;
+    // Set the position of the tag element
+    arTag.style.top = `${screenPosition.y}px`;
+    arTag.style.left = `${screenPosition.x}px`;
 }
 
 // Call the function when the AR object is spawned or moves
-arObject.addEventListener('object3dset', updateUIPosition);
-arObject.addEventListener('positionchanged', updateUIPosition);
+arObject.addEventListener('object3dset', updateTagPosition);
+arObject.addEventListener('positionchanged', updateTagPosition);
 
 // Initial positioning
-updateUIPosition();
+updateTagPosition();
